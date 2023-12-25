1ba2f308dc8d654d7e08c439490e09d099325981
louis frederic schefer <lf.schefer@gmail.com>
2023-12-25 12:16:52 +0100

    full search

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,35 +5,67 @@ import FilmCard from "./FilmCard";
 
 export default function Results(props) {
 
-  const [apiResult, setApiResult] = React.useState({responseData:null,ready:false})
+  const [ready, setReady] = React.useState(true)
+  const [apiResult, setApiResult] = React.useState({responseData:[], ready: true})
+  const [pageNumb, setPageNumb] = React.useState(1)
 
-  const url = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${props.searchText}`;
+  const title = props.searchText !== '' ? `&s=${props.searchText}` : '&s=Batman';
+  const year = props.year !=='' ? `&y=${props.year}` :'';
+  const type = `&type=${props.type}`
+  const page = `&page=${pageNumb}`
 
+  const url = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}${title}${year}${type}${page}`;
 
   React.useEffect(() => {
     async function fetchApi() {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(apiResult.responseData)
       if (data.Response === 'False') {
-        setApiResult({ready:false})
+        setReady(false)
       }
       else {
-        setApiResult({responseData:data.Search,ready:true});
+        setApiResult({responseData:data.Search});
+        setReady(true)
       }
     }
     fetchApi()
-  },[apiResult.responseData, props.searchText, url])
+  },[url, ready])
 
-const films = apiResult.ready ? apiResult.responseData.map((film, i) => {
-  return <FilmCard
-  film={film}
-  key={i}/>
-}) : "";
+  React.useEffect(()=> {
+    setPageNumb(1)
+  },[title,year,type])
+
+  const incrementPage = () => {
+    setPageNumb(prev => prev + 1)
+  };
+
+  const decrementPage = () => {
+    if (pageNumb>1) {
+      setPageNumb(prev => prev -1)
+    }
+  };
+
+
+  const films = apiResult.responseData.map((film, i) => {
+    return <FilmCard
+    film={film}
+    key={i}/>
+  })
+
+  const textResult = ready ? <h3>Results:</h3> : <div><h3>No match</h3><hr /><h5>Your previous résutls</h5></div>;
+
+  const btnPages = <div className="btn-container">
+    <div className="btn" onClick={decrementPage}>Previous page</div>
+    <p>Pages: {pageNumb}</p>
+    <div className="btn" onClick={incrementPage}>Next page</div>
+  </div>
+
+  console.log(pageNumb)
 
   return(
     <>
-    <h3>Results:</h3>
+    {textResult}
+    {btnPages}
     {films}
     </>
   );
--- src/components/Search-bar.jsx
+++ src/components/Search-bar.jsx
@@ -4,9 +4,9 @@ import Results from "./Results";
 
 export default function Search(props) {
 
-  const [formInput, setFormInput] = React.useState({searchText:"batman"})
+  const [formInput, setFormInput] = React.useState({searchText:"", year:'', type:'movie'})
 
-  const handleChange = (event) => {
+  const inputTitle = (event) => {
     setFormInput((prev) => {
       return { ...prev,
         searchText: event
@@ -14,9 +14,36 @@ export default function Search(props) {
     })
   };
 
-  const searchForm = <form action="">
-    <input type="text" name="search" onChange={(event) => handleChange(event.target.value)} value={formInput.searchText}/>
+  const inputYear = (event) => {
+    setFormInput((prev) => {
+      return { ...prev,
+        year: event
+      }
+    })
+  };
+
+  const inputType = (event) => {
+    setFormInput((prev) => {
+      return {...prev,
+      type: event}
+    })
+  };
+
+  const searchForm =
+  <form action="">
+    <label htmlFor="">Title:</label>
+    <input type="text" name="search" placeholder="Batman" onChange={(event) => inputTitle(event.target.value)} value={formInput.searchText}/>
+    <label htmlFor="">Year:</label>
+    <input type="text" name="year" placeholder="2014" onChange={(event) => inputYear(event.target.value)} value={formInput.year}/>
+    <label htmlFor="">Type:</label>
+    <select name="type" onChange={(event) => inputType(event.target.value)} value={formInput.type}>
+      <option value="movie">movie</option>
+      <option value="series">series</option>
+      <option value="episode">episode</option>
+    </select>
   </form>
+
+
 
   return(
     <>
@@ -24,6 +51,8 @@ export default function Search(props) {
     {searchForm}
     < Results
     searchText={formInput.searchText}
+    year={formInput.year}
+    type={formInput.type}
     />
     </>
   );
